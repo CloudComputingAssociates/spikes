145222576cd131a8efbfabfb4512deca799efa05
martymazurik <marty@cloudcomputingassociates.com>
2016-03-31 14:17:30 -0700

    updated echo to use concat strings and vars

--- build/spikes_deploy.sh
+++ build/spikes_deploy.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # env vars
-echo "Env vars... "
-set DEV = '/home/dev/webs'
-echo "DEV: {$DEV}"
-set JENKINS_HOME = '/var/lib/jenkins'
-echo "JENKINS_HOME: {$JENKINS_HOME}"
+message = "Env vars... "
+echo message
+
+DEV = "/home/dev/webs"
+message = "DEV: "
+message += $DEV
+echo message
+
+JENKINS_HOME = "/var/lib/jenkins"
+message = "JENKINS_HOME: "
+message += $JENKINS_HOME
+echo message
 
 # nuke everything in DEV
 echo "Nuke everything ..."
 rm -rfv ${DEV}
 
 # Spikes - typescript HelloWorld example, 1-page app
-echo "Moving typescript HelloWorld example ..."
+message = "Moving typescript HelloWorld example ..."
+echo message
 mkdir {$DEV}/HelloWorld
 cp ${JENKINS_HOME}/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.cs ${DEV}/HelloWorld/
 cp ${JENKINS_HOME}/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.js ${DEV}/HelloWorld/
