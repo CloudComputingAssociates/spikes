6ca2f11ad6d011eb38e64108aeac94edf65466ad
martymazurik <marty@cloudcomputingassociates.com>
2016-03-31 14:35:56 -0700

    fix vars added $'s and removed curleys

--- build/spikes_deploy.sh
+++ build/spikes_deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # env vars
 message="Env vars... "
-echo message
+echo $message
 
 DEV="/home/dev/webs"
 message="DEV: "
 message+=$DEV
-echo message
+echo $message
 
 JENKINS_HOME="/var/lib/jenkins"
 message="JENKINS_HOME: "
 message+=$JENKINS_HOME
-echo message
+echo $message
 
 # nuke everything in DEV
 echo "Nuke everything ..."
@@ -19,10 +19,10 @@ rm -rfv ${DEV}
 
 # Spikes - typescript HelloWorld example, 1-page app
 message="Moving typescript HelloWorld example ..."
-echo message
-mkdir {$DEV}/HelloWorld
-cp ${JENKINS_HOME}/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.cs ${DEV}/HelloWorld/
-cp ${JENKINS_HOME}/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.js ${DEV}/HelloWorld/
-cp ${JENKINS_HOME}/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/index.html ${DEV}/HelloWorld/
+echo $message
+mkdir $DEV/HelloWorld
+cp $JENKINS_HOME/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.cs ${DEV}/HelloWorld/
+cp $JENKINS_HOME/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/app.js ${DEV}/HelloWorld/
+cp $JENKINS_HOME/jobs/Spikes/workspace/typescript/HelloWorld/HelloWorld/index.html ${DEV}/HelloWorld/
 echo "done."
 
