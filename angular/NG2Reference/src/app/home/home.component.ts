e556f29f7b1592c937c6bb29beaa247fde6dfaff
martymazurik <marty@cloudcomputingassociates.com>
2016-11-22 11:49:39 -0800

    Added ReadMe.txt

--- angular/NG2Reference/src/app/home/home.component.ts
+++ angular/NG2Reference/src/app/home/home.component.ts
@@ -1,35 +1,48 @@
 import { Component, OnInit} from '@angular/core';
-import { Http } from '@angular/http'; //mtm 11/11/2016; added to reference http module
+import { Http, Response } from '@angular/http'; //mtm 11/11/2016; added to reference http module
 import { ApiService } from '../shared';
 import { Observable } from 'rxjs/Rx';
 
 
-export interface ComputeInstance {
-    name: string;
-}
-
-
 @Component({
   selector: 'my-home',
-  templateUrl: './home.component.html',
+  template: `<p> Home Works! <p>
+             <div>
+                <table border="1">
+                <tr *ngFor="let u of users">
+                    <td><span>{{u.Name }}</span></td>
+                </tr>
+                </table>
+                <button (click)="clicked()" id="btnUserDetails">Invoke User Details Service</button>
+              </div>`
+  ,
   styleUrls: ['./home.component.scss'],
-  providers: [ApiService]                   // key linkage **don't forget 
+ // providers: [ApiService]                   // key linkage **don't forget 
 })
 export class HomeComponent implements OnInit {
+
+    users: Array<any>;
 
     computeInstances: ComputeInstance[] = []; // [ { "name" : "server1" }, { "name" : "server2" }]; 
     errorMessage: string;
 
-    constructor(private api: ApiService) { }
+    constructor(private http: Http) { }
 
     public clicked() {
-
+        this.getComputeInstances();
     }
+
+    url: string = "http://dogservice.azurewebsites.net";
+
+    getComputeInstances() {
+        this.http.get(this.url)
+            .subscribe((res: Response) => {
+                this.users = res.json();
+            });
+    }
+
+
     ngOnInit() {
-        this.api
-            .getComputeInstances()
-            .subscribe(ci => this.computeInstances = ci,
-                       error => this.errorMessage = <any>error
-        );
+
     }
 }
--- angular/NG2Reference/src/app/shared/api.service.ts
+++ angular/NG2Reference/src/app/shared/api.service.ts
@@ -10,29 +10,34 @@ export interface ComputeInstance {
 @Injectable() export class ApiService {
 
     title = 'Angular 2 - REST Api Call Example';
+    public users: Observable<Array<any>>;
 
-    endPointURL: string = "http://dogservice.azurewebsites.net/api/values";
+    // endPointURL: string = "http://dogservice.azurewebsites.net/api/values";
+    url: string = "http://jsonplaceholder.typicode.com/posts";
 
     constructor(private http: Http) { }
 
-    getComputeInstances() : Observable<ComputeInstance[]>{
-        return this.http.get(this.endPointURL, { headers: this.getHeaders() }).map(mapComputeInstances)
+    getComputeInstances() {
+        this.http.get(this.url)
+            .subscribe((res: Response) => {
+                this.users = res.json();
+            });
     }
-    private getHeaders() {
-        let headers = new Headers();
-        headers.append('Accept', 'application/json');
-        return headers;
-    }
+    //private getHeaders() {
+    //    let headers = new Headers();
+    //    headers.append('Accept', 'application/json');
+    //    return headers;
+    //}
 }
 
-function mapComputeInstances(response: Response): ComputeInstance[] { 
-    return response.json().results.map(unpackComputeInstance)
-}
+//function mapComputeInstances(response: Response): ComputeInstance[] { 
+//    return response.json().results.map(unpackComputeInstance)
+//}
 
-function unpackComputeInstance(item: any): ComputeInstance {
-    let computeInstance = <ComputeInstance>({    // see toPerson(), for more mapping ideas; https://www.barbarianmeetscoding.com/blog/2016/04/02/getting-started-with-angular-2-step-by-step-6-consuming-real-data-with-http/
-        name: item.Name
-    });                                         
-    return computeInstance;
-}
+//function unpackComputeInstance(item: any): ComputeInstance {
+//    let computeInstance = <ComputeInstance>({    // see toPerson(), for more mapping ideas; https://www.barbarianmeetscoding.com/blog/2016/04/02/getting-started-with-angular-2-step-by-step-6-consuming-real-data-with-http/
+//        name: item.Name
+//    });                                         
+//    return computeInstance;
+//}
 
